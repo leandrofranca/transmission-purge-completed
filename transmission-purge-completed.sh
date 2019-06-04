0436f48a6e466809b99864f7e704a681b0a9dd03
Leandro França <leandrofranca@gmail.com>
2019-06-04 00:14:20 -0300

    Working with broken torrents

--- transmission-purge-completed.sh
+++ transmission-purge-completed.sh
@@ -9,12 +9,16 @@ do
     echo "## Operations on torrent ID $TORRENTID starting ##"
     
     DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100"`
+    DL_MOVED=`transmission-remote --torrent $TORRENTID --info | grep "Error: No data found!"`
     
     if [ "$DL_COMPLETED" != "" ]; then
         echo "Torrent #$TORRENTID is completed."
         echo "Removing torrent from list."
-
+        
         transmission-remote --torrent $TORRENTID --remove-and-delete
+    elif [ "$DL_MOVED" != "" ]; then
+        echo "Torrent #$TORRENTID is broken. Restarting..."
+        transmission-remote --torrent $TORRENTID --verify --start
     else
         echo "Torrent #$TORRENTID is not completed. Ignoring."
     fi
