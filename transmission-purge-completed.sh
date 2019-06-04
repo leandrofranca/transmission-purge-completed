832d681d94687e084a607eac1b2f22b7faf17f15
Leandro França <leandrofranca@gmail.com>
2019-06-03 23:46:08 -0300

    Changing script to perform with Kodi plugin

--- transmission-purge-completed.sh
+++ transmission-purge-completed.sh
@@ -1,31 +1,22 @@
 #!/bin/sh
 
-TORRENTLIST=`transmission-remote --auth=transmission:transmission --list | sed -e '1d;$d;s/^ *//;s/*//' | cut --only-delimited --delimiter=' ' --fields=1`
-PATH_LOG=~
+TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//;s/*//' | cut -s -d ' ' -f 1`
 
-transmission-remote --auth=transmission:transmission --list
+transmission-remote --list
 
 for TORRENTID in $TORRENTLIST
 do
+    echo "## Operations on torrent ID $TORRENTID starting ##"
+    
+    DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100"`
+    
+    if [ "$DL_COMPLETED" != "" ]; then
+        echo "Torrent #$TORRENTID is completed."
+        echo "Removing torrent from list."
 
-echo "## Operations on torrent ID $TORRENTID starting ##"
-
-DL_COMPLETED=`transmission-remote --auth=transmission:transmission -t $TORRENTID --info | grep "Percent Done: 100%"`
-DL_MOVED=`transmission-remote --auth=transmission:transmission -t $TORRENTID --info | grep "Error: No data found!"`
-
-if [ "$DL_COMPLETED" != "" ] || [ "$DL_MOVED" != "" ]; then
-  echo "Torrent #$TORRENTID is completed."
-  echo "Removing torrent from list."
-
-  DL_NAME=`transmission-remote --auth=transmission:transmission -t $TORRENTID --info | grep "Name:"`
-  DL_LAST_ACTIVITY=`transmission-remote --auth=transmission:transmission -t $TORRENTID --info | grep "Latest activity:"`
-  DL_PUSH="$DL_NAME || $DL_LAST_ACTIVITY"
-
-  echo $DL_PUSH >> $PATH_LOG/transmission-purge-completed.log
-
-  transmission-remote --auth=transmission:transmission --torrent $TORRENTID --remove
-else
-  echo "Torrent #$TORRENTID is not completed. Ignoring."
-fi
-  echo "## Operations on torrent ID $TORRENTID completed ##"
+        transmission-remote --torrent $TORRENTID --remove-and-delete
+    else
+        echo "Torrent #$TORRENTID is not completed. Ignoring."
+    fi
+    echo "## Operations on torrent ID $TORRENTID completed ##"
 done
