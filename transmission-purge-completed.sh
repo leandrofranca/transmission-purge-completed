2c5be3a32944742d90b19b3a9785b4b58fdc859a
Leandro França <leandrofranca@gmail.com>
2019-06-16 14:04:05 -0300

    Update Kodi library after download

--- transmission-purge-completed.sh
+++ transmission-purge-completed.sh
@@ -1,26 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-TORRENTLIST=`transmission-remote --list | sed -e '1d;$d;s/^ *//;s/*//' | cut -s -d ' ' -f 1`
+TORRENTLIST=`/storage/.kodi/addons/service.transmission/bin/transmission-remote --list | sed -e '1d;$d;s/^ *//;s/*//' | cut -s -d ' ' -f 1`
 
-transmission-remote --list
+/storage/.kodi/addons/service.transmission/bin/transmission-remote --list
 
 for TORRENTID in $TORRENTLIST
 do
-    echo "## Operations on torrent ID $TORRENTID starting ##"
-    
-    DL_COMPLETED=`transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100"`
-    DL_MOVED=`transmission-remote --torrent $TORRENTID --info | grep "Error: No data found!"`
+    DL_NAME=`/storage/.kodi/addons/service.transmission/bin/transmission-remote --torrent $TORRENTID --info | grep "Name: " | cut -s -d ' ' -f 4`
+    DL_COMPLETED=`/storage/.kodi/addons/service.transmission/bin/transmission-remote --torrent $TORRENTID --info | grep "Percent Done: 100"`
+    DL_MOVED=`/storage/.kodi/addons/service.transmission/bin/transmission-remote --torrent $TORRENTID --info | grep "Error: No data found!"`
     
     if [ "$DL_COMPLETED" != "" ]; then
-        echo "Torrent #$TORRENTID is completed."
-        echo "Removing torrent from list."
+        echo "Torrent $DL_NAME is completed. Removing torrent from list..."
         
-        transmission-remote --torrent $TORRENTID --remove-and-delete
+        /storage/.kodi/addons/service.transmission/bin/transmission-remote --torrent $TORRENTID --remove-and-delete
+        ( sleep 1m ; \
+            rm -rf "/media/sda-usb-Mass_Storage_Dev/downloads/$DL_NAME"
+            curl --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' http://192.168.31.40:8080/
+        ) &
     elif [ "$DL_MOVED" != "" ]; then
-        echo "Torrent #$TORRENTID is broken. Restarting..."
-        transmission-remote --torrent $TORRENTID --verify --start
+        echo "Torrent $DL_NAME is broken. Restarting..."
+        /storage/.kodi/addons/service.transmission/bin/transmission-remote --torrent $TORRENTID --verify --start
     else
-        echo "Torrent #$TORRENTID is not completed. Ignoring."
+        echo "Torrent $DL_NAME is not completed. Ignoring..."
     fi
-    echo "## Operations on torrent ID $TORRENTID completed ##"
 done
